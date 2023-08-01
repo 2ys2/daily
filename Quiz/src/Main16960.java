d5e5ab83cc82b35b862ae51594059ce14d3176a0
2ys2 <2ys2bau@gmail.com>
2023-08-01 09:03:54 +0900

    daily

--- Quiz/src/Main16960.java
+++ Quiz/src/Main16960.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main16960 {
-	static int[][] arr;
 	static int N, M;
+	static int[] numset;
+	static ArrayList<Integer>[] list;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,37 +16,49 @@ public class Main16960 {
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); 
+		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
-		int[] cnt = new int[M];
 
-		for (int i = 0; i < N; i++) {
+		list = new ArrayList[N+1];
+		numset = new int[Math.max(N, M)+1];
+		for (int i = 1; i <= N; i++ ) {
+			list[i] = new ArrayList<>();
 			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < n; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				arr[i][num-1] = num;
-				cnt[num-1]++;
+			int num = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < num; j++ ) {
+				int temp = Integer.parseInt(st.nextToken());
+				list[i].add(temp);
+				numset[temp]++;
 			}
 		}
+		
+		System.out.println(Arrays.toString(numset));
 
-		System.out.println(Arrays.toString(cnt));
-
-
-		System.out.println(Arrays.toString(cnt));
-		for(int i = 0; i < arr.length; i++ ) {
-			for (int j = 0; j<arr[i].length; j++) {
-				System.out.print(arr[i][j]);
+		for (int i = 1; i <= N; i++ ) {
+			if ( onlamp(i) == 1 ) {
+				System.out.println(1);
+				return;
 			}
-			System.out.println();
 		}
+		System.out.println(0);
 
 
 	}
+
+	private static int onlamp(int index) {
+		int result = 1;
+		for (int num : list[index]) {
+			numset[num]--;
+			if (numset[num] <= 0 ) {
+				result = 0;
+			}
+		}
+		
+		for (int num : list[index]) {
+			numset[num]++;
+		}
+		
+		return result;
+	}
 }
-
-
-
-
 
---
+++ Quiz/src/Main26099.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main26099 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		long cnt = 0;
+		
+		if ( N == 4 || N == 7) {
+			cnt = -1;
+		}else if ( N % 5 == 0){
+			cnt = (N / 5);
+			
+		}else if ( N % 5 == 1){
+			cnt = (N / 5) + 1;
+		}else if ( N % 5 == 2){
+			cnt = (N / 5) + 2;
+		}else if ( N % 5 == 3){
+			cnt = (N / 5) + 1;
+		}else if ( N % 5 == 4){
+			cnt = (N / 5) + 2;
+		}
+		System.out.println(cnt);
+	}
+}
